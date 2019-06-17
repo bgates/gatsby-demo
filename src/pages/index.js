5f7e81c1d574b84d4a553687243a6a3b0299a839
Brian Gates <briandotgates@gmail.com>
2019-06-16 21:24:08 -0400

    show Link component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <section>
+    <h1>Hello world!</h1>
+    <p>
+      Open the DevTools Network tab and watch what happens when you mouse over a
+      link.
+    </p>
+    <ul>
+      <li>
+        <Link to="page-2">Page 2</Link>
+      </li>
+      <li>
+        <Link to="page-3">Page 3</Link>
+      </li>
+      <li>
+        <Link to="page-4">Page 4</Link>
+      </li>
+    </ul>
+  </section>
+)
---
+++ src/pages/page-2.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default () => (
+  <section>
+    <div>Page 2</div>
+    <p>
+      Running this in a development environment obscures what the Link component
+      is doing.{" "}
+      <a href="https://gatsby-starter-default-demo.netlify.com/">
+        Follow this link
+      </a>{" "}
+      to see how Link works in production.
+    </p>
+  </section>
+)
